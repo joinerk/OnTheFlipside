980ca3cb8e8f1b8b1ef45eaf7bdb6c42c223e720
Leigh <joinerk1@newpaltz.edu>
2024-10-16 18:17:22 -0400

    Doesn't commit to git

--- Scenes/game.gd
+++ Scenes/game.gd
@@ -8,3 +8,6 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+func _on_button_pressed():
+	get_tree().change_scene_to_file("res://Levels/level_1_other.tscn")
