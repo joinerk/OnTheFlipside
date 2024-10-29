87186a171692589f00756ad0b5bbff47114c5d0f
Leigh <joinerk1@newpaltz.edu>
2024-10-29 15:58:33 -0400

    Fixed Other crate

--- Scripts/game.gd
+++ Scripts/game.gd
@@ -1,5 +1,6 @@
 extends Node2D
 
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -10,6 +11,14 @@ func _process(_delta: float) -> void:
 	pass
 
 
-
+# Button in top corner
 func _on_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://Levels/level_1_other.tscn")
+
+# Trying to get TAB to work - not working
+func _switch():
+	if Input.is_action_just_pressed("switch"):
+		get_tree().change_scene_to_file("res://Levels/level_1_other.tscn")
+
+
+# Pausing opjects for switching
--- Scripts/player_2.gd
+++ Scripts/player_2.gd
@@ -42,10 +42,14 @@ func _physics_process(delta: float) -> void:
 	move_and_slide()
 
 
+
+
+
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Rigidbody"):
 		body.collision_layer = 1
 		body.collision_mask = 1
+
 
 func _on_area_2d_body_exited(body: Node2D) -> void:
 	if body.is_in_group("Rigidbody"):
